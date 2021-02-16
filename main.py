9e8c6b98554e1cf164a53399664b60ab0ca08c09
pichu0357 <nachinarchess96@gmail.com>
2021-02-16 18:48:15 +0530

    comments added

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
-from hata import Client
+from hata import Client, Guild
 from hata.ext.commands import setup_ext_commands
 from hata.ext.slash import setup_ext_slash
 from dotenv import load_dotenv
 import os
 
+# loading the .env file
 load_dotenv()
 Token = os.environ.get('Token')
 APPLICATION_ID = os.environ.get('APPLICATION_ID')
+GUILD = Guild.precreate(os.environ.get('GUILD_ID'))
 
 Pichu = Client(Token, application_id=APPLICATION_ID)
 setup_ext_commands(Pichu, '$')
 setup_ext_slash(Pichu)
 
+
 @Pichu.events
 async def ready(client):
     print(f'{client:f} logged in.')
+
 
 @Pichu.events
 async def message_create(client, message):
@@ -26,8 +30,9 @@ async def message_create(client, message):
     if lowercase_conversion == 'ping':
         await client.message_create(message.channel, 'pong')
 
+
 for commands in os.listdir("./commands/"):
-    if (commands.endswith('.py')):
+    if commands.endswith('.py'):
         with open(f'commands/{commands}') as rk:
             exec(rk.read())
 
