e3c97d57f1bbbe4bb6d340ed2fa56e81289d5f60
pichu0357 <nachinarchess96@gmail.com>
2021-05-22 17:51:54 +0530

    old code

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from hata import Client, Guild
 from hata.ext.commands import setup_ext_commands
 from hata.ext.slash import setup_ext_slash
+from hata.ext.extension_loader import EXTENSION_LOADER
 from dotenv import load_dotenv
 import os
 
 # loading the .env file
 load_dotenv()
 
-Token = os.environ.get('Token')
-APPLICATION_ID = os.environ.get('APPLICATION_ID')
+TOKEN = os.environ.get('TOKEN')
 GUILD = Guild.precreate(os.environ.get('GUILD_ID'))
 
-Pichu = Client(Token, application_id=APPLICATION_ID)
+Pichu = Client(TOKEN)
 setup_ext_commands(Pichu, '$')
 setup_ext_slash(Pichu)
 
@@ -32,9 +32,12 @@ async def message_create(client, message):
         await client.message_create(message.channel, 'pong')
 
 
-for commands in os.listdir("./commands/"):
-    if commands.endswith('.py'):
-        with open(f'commands/{commands}') as rk:
-            exec(rk.read())
+EXTENSION_LOADER.add_default_variables(Pichu=Pichu)
 
+for command in os.listdir('commands'):
+    if command.endswith('.py'):
+        EXTENSION_LOADER.add(f'commands.{command[:-3]}', Pichu=Pichu)
+
+
+EXTENSION_LOADER.load_all()
 Pichu.start()
